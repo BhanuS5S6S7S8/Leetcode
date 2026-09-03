6e5cf8ddd8b21c7ff19de2608f2c43108f5cf1d2
Bhanu <26singhbhanu@gmail.com>
2026-09-03 21:14:05 +0530

    Time: 6 ms (91.96%), Space: 46.6 MB (40.83%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -7,16 +7,16 @@ class Solution {
             return a[0] - b[0];
         });
 
-        int count = 1;
+        int cou = 1;
         int end = intervals[0][1];
 
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][1] > end) {
-                count++;
+                cou++;
                 end = intervals[i][1];
             }
         }
 
-        return count;
+        return cou ;
     }
 }
