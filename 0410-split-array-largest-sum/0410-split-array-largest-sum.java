497b2d0f888a44fcdcb123a9b539c4d23432ca68
Bhanu <26singhbhanu@gmail.com>
2026-09-15 19:10:43 +0530

    Time: 1 ms (57.6%), Space: 43 MB (50.38%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int min = nums[0] ; 
+        int max = 0 ;
+        for(int i = 0 ; i < nums.length ; i++){
+            max += nums[i] ; 
+            if(nums[i] > min){
+                min = nums[i] ; 
+            }
+        } 
+        int ans = 0 ;
+        while(min <= max){
+            int mid = ( min + max )/2 ; 
+            if(isAble(mid , k , nums)){
+                ans = mid ; 
+                max = mid-1 ;
+            }else{
+                min = mid+1 ; 
+            }
+        }
+        return ans ; 
+    }
+    public boolean isAble(int mid , int k , int[] nums){
+        int stu = 1 ; 
+        int sum = 0 ; 
+        for(int i = 0 ; i < nums.length ; i++ ){
+            if((sum + nums[i] ) > mid ){
+                stu++ ; 
+                sum = nums[i] ; 
+            }else{
+                sum += nums[i] ;
+            }
+        }
+
+        return ( stu <= k ) ;
+    }
+}
