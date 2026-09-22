8c278dad299a053d82edbd6ef43cbcffbdfca88b
Bhanu <26singhbhanu@gmail.com>
2026-09-22 20:27:48 +0530

    Time: 22 ms (43.95%), Space: 52.7 MB (20.18%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,20 +1,21 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int k) {
-        int currsum = 0 ;
-        int start = 0 ;  
-        int i = 0 ; 
-        int ans = 0 ; 
-        HashMap<Integer , Integer> hm = new HashMap<>() ; 
-        hm.put(0,1) ; 
+        int sum = 0 ; 
+        int j = 0 ; 
+        int ans = 0 ;
 
-        while(i < nums.length){
-            currsum += nums[i] ;
-            if(hm.containsKey(currsum - k)){
-                ans += hm.get(currsum - k) ; 
+        HashMap<Integer , Integer> hm = new HashMap<>() ;
+        hm.put(0,1) ;
+
+        while(j < nums.length){
+            sum += nums[j] ; 
+            if(hm.containsKey(sum - k)){
+                ans += hm.get(sum-k) ; 
             }
-            hm.put(currsum , hm.getOrDefault(currsum , 0) + 1) ; 
-            i++ ; 
+            hm.put(sum , hm.getOrDefault(sum , 0 ) + 1 ) ;
+            j++ ;  
         }
+
         return ans ; 
     }
 }
